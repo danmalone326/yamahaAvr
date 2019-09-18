ba2378ddb34120d3721ccf1c7b6158411abdb7aa
Dan Malone <dmalone@calpoly.edu>
2019-09-17 20:14:55 -0700

    adding more methods.

--- yamahaAvr.py
+++ yamahaAvr.py
@@ -4,6 +4,9 @@ import json
 
 debug=True
 
+def printJSON(data):
+    print(json.dumps(data, indent=4, sort_keys=True)) 
+
 def request(url,data=None,headers=None):
     if debug:
         print url
@@ -32,7 +35,8 @@ def request(url,data=None,headers=None):
 
     if debug:
         print "-"
-        print(json.dumps(responseData, indent=4, sort_keys=True))
+        print responseCode[responseData['response_code']]
+        printJSON(responseData)
         print "-"
     
     return responseData
@@ -47,15 +51,47 @@ def postJSON(url,jsonData):
     # headers
     headers = {'Content-Type': 'application/json'}
 
-    return self.request(url,data,headers)
+    return request(url,data,headers)
 
-
+responseCode = {
+    0: "Successful request",
+    1: "Initializing",
+    2: "Internal Error",
+    3: "Invalid Request",
+    4: "Invalid Parameter",
+    5: "Guarded",
+    6: "Time Out",
+    99: "Firmware Updating",
+# (100s are Streaming Service related errors)
+    100: "Access Error",
+    101: "Other Errors",
+    102: "Wrong User Name",
+    103: "Wrong Password",
+    104: "Account Expired",
+    105: "Account Disconnected/Gone Off/Shut Down",
+    106: "Account Number Reached to the Limit",
+    107: "Server Maintenance",
+    108: "Invalid Account",
+    109: "License Error",
+    110: "Read Only Mode",
+    111: "Max Stations",
+    112: "Access Denied"
+}
 
 class YamahaAVR:
     def __init__(self,host,debug=False):
         self.debug = debug
         self.host = host
         self.urlPrefix="http://%s/" % (self.host)
+        
+    def getFeatures(self):
+        result=getURL(self.urlPrefix+"YamahaExtendedControl/v1/system/getFeatures")
+
+    def getStatus(self):
+        result=getURL(self.urlPrefix+"YamahaExtendedControl/v1/main/getStatus")
+        self.power = result['power']
+        self.input = result['input']
+
 
     def resetMenu(self):
         while True:
